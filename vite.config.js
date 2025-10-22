c7c938428d60ccfd042c7568295551f8475451b6
RolandoMamani <rolomc2016@gmail.com>
2025-10-22 18:13:31 -0500

    actualizo formulario

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/formulario/', // 👈 ESTA LÍNEA es la clave para GitHub Pages
 })
